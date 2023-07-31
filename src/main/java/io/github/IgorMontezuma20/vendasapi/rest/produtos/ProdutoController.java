007ac5b6bb7f99f1829dee42bec74f5060587cda
IgorMontezuma20 <igorminranda17@gmail.com>
2023-07-31 11:01:19 -0300

    feat: criando metodo de atuaÃlizar produto

--- src/main/java/io/github/IgorMontezuma20/vendasapi/rest/produtos/ProdutoController.java
+++ src/main/java/io/github/IgorMontezuma20/vendasapi/rest/produtos/ProdutoController.java
@@ -3,7 +3,12 @@ package io.github.IgorMontezuma20.vendasapi.rest.produtos;
 import io.github.IgorMontezuma20.vendasapi.model.Produto;
 import io.github.IgorMontezuma20.vendasapi.model.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/produtos")
@@ -19,5 +24,19 @@ public class ProdutoController {
         Produto produtoEntity = produto.toModel();
         repository.save(produtoEntity);
         return ProdutoFormRequest.fromModel(produtoEntity);
+    }
+    @PutMapping("{id}")
+    public ResponseEntity<Void> atualizar(@PathVariable Long id, @RequestBody ProdutoFormRequest produto){
+        Optional<Produto> produtoExistente = repository.findById(id);
+
+        if (produtoExistente.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+
+        Produto entidade = produto.toModel();
+        entidade.setId(id);
+        repository.save(entidade);
+
+        return ResponseEntity.ok().build();
     }
 }
