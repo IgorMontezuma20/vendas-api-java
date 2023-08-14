4706d71e654f282682440c4c461f9cf56dc8528d
IgorMontezuma20 <igorminranda17@gmail.com>
2023-08-13 21:20:13 -0300

    feat: implementando a logica para criar o relatorio em pdf

--- src/main/java/io/github/IgorMontezuma20/vendasapi/service/RelatorioVendasService.java
+++ src/main/java/io/github/IgorMontezuma20/vendasapi/service/RelatorioVendasService.java
@@ -1,11 +1,46 @@
 package io.github.IgorMontezuma20.vendasapi.service;
 
+import net.sf.jasperreports.engine.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class RelatorioVendasService {
 
+    @Value("classpath:reports/~relatorio-vendas.jrxml")
+    private Resource relatorioVendas;
+
+    @Autowired
+    private DataSource dataSource;
+
     public byte[] gerarRelatorio(){
+
+        //try with resources
+        try (Connection connection = dataSource.getConnection()){
+            JasperReport compiledReport = JasperCompileManager
+                    .compileReport(relatorioVendas.getInputStream());
+            Map<String, Object> parametros = new HashMap<>();
+            JasperPrint print = JasperFillManager
+                    .fillReport(compiledReport, parametros, connection);
+
+            return JasperExportManager.exportReportToPdf(print);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (JRException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         return null;
     }
 }
