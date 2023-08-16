a4fd07c19a659060b6b9ef940bf4a5720d30e8ac
IgorMontezuma20 <igorminranda17@gmail.com>
2023-08-16 15:40:10 -0300

    feat: adicionando metodo para apresentar os  meses vazios

--- src/main/java/io/github/IgorMontezuma20/vendasapi/rest/dashboard/DashBoardData.java
+++ src/main/java/io/github/IgorMontezuma20/vendasapi/rest/dashboard/DashBoardData.java
@@ -2,7 +2,12 @@ package io.github.IgorMontezuma20.vendasapi.rest.dashboard;
 
 import io.github.IgorMontezuma20.vendasapi.model.repository.projections.VendaPorMes;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class DashBoardData {
 
@@ -16,6 +21,7 @@ public class DashBoardData {
         this.clientes = clientes;
         this.vendas = vendas;
         this.vendasPorMes = vendasPorMes;
+        this.preencherMesesFaltantes();
     }
 
     public Long getProdutos() {
@@ -47,6 +53,46 @@ public class DashBoardData {
     }
 
     public void setVendasPorMes(List<VendaPorMes> vendasPorMes) {
+        if(vendasPorMes==null){
+            vendasPorMes = new ArrayList<>();
+        }
         this.vendasPorMes = vendasPorMes;
+    }
+
+    public void preencherMesesFaltantes(){
+       Integer mesMaximo =  getVendasPorMes()
+               .stream()
+               .mapToInt(VendaPorMes::getMes)
+               .max().getAsInt();
+
+       List<Integer> listaMeses =  IntStream
+               .rangeClosed(1, mesMaximo)
+               .boxed()
+               .collect(Collectors.toList());
+
+
+       List<Integer> mesesAdicionados = getVendasPorMes()
+               .stream()
+               .map(VendaPorMes::getMes)
+               .collect(Collectors.toList());
+
+       listaMeses.stream().forEach(mes -> {
+           if(!mesesAdicionados.contains(mes)){
+               VendaPorMes vendaPorMes = new VendaPorMes() {
+                   @Override
+                   public Integer getMes() {
+                       return mes;
+                   }
+
+                   @Override
+                   public BigDecimal getValor() {
+                       return BigDecimal.ZERO;
+                   }
+               };
+                getVendasPorMes().add(vendaPorMes);
+           }
+       });
+
+       getVendasPorMes().sort( Comparator.comparing(VendaPorMes::getMes) );
     }
 }
