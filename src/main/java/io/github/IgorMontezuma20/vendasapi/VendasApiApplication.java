a51f179ad03872162f605e00278a443083d8bd01
IgorMontezuma20 <igorminranda17@gmail.com>
2023-08-17 11:49:46 -0300

    feat: preparando o ambiente para a inicializacao do build da aplicacao

--- src/main/java/io/github/IgorMontezuma20/vendasapi/VendasApiApplication.java
+++ src/main/java/io/github/IgorMontezuma20/vendasapi/VendasApiApplication.java
@@ -1,7 +1,9 @@
 package io.github.IgorMontezuma20.vendasapi;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,10 +11,21 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class VendasApiApplication {
-	
-	@GetMapping("/hello/{nome}")
-	public String hello(@PathVariable String nome) {
-		return "Hello " + nome + "!";
+
+	@Autowired
+	private Environment env;
+
+	@GetMapping("/")
+	public String getAmbiente(){
+		String ambienteAtual= "PADRÃO (nenhum)";
+
+		if(env.getActiveProfiles().length > 0){
+			ambienteAtual = env.getActiveProfiles()[0];
+		}
+
+		String appAName = env.getProperty("spring.application.name");
+
+		return String.format("Ambiente: %s | App Name:  %s", ambienteAtual,appAName);
 	}
 
 	public static void main(String[] args) {
